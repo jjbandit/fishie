8bbb5d1cee7b21efae74f98642542a9739191bf3
jjbandit <revelstokesnowboard@gmail.com>
2015-01-07 15:12:54 -0800

    add classes

--- fishie.js
+++ fishie.js
@@ -1,23 +1,48 @@
+Classes = new Mongo.Collection("classes");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+	// Meteor.subscribe("classes");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+	Template.body.helpers ({
+		classes: function() {
+			return Classes.find();
+		}
 
-  Template.hello.events({
+	});
+
+  Template.newClassButtons.events({
+
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
+			level = event.target.classList.item(1);
+
+			// level.forEach(getLevel);
+			
+			// function getLevel (element, index, array) {
+			// 	console.log(index + element);
+			// }
+
+			Meteor.call ("addClass", level);
+		}
   });
 }
+
+Meteor.methods ({
+	addClass: function(level) {
+		Classes.insert ({
+			level: level,
+			createdAt: new Date()
+		});
+			console.log(Classes.find().count());
+			console.log(Classes.findOne({}, {skip: Classes.find().count()-1})); 
+	} 
+}); 
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+	// Meteor.publish("classes",  function() {
+	// 	return Classes.find()
+	// 		});
 }
