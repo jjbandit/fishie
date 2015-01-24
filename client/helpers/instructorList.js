b40e3da1e60a15641b05d93a3468eb448ed12f89
jjbandit <revelstokesnowboard@gmail.com>
2015-01-23 21:51:52 -0800

    make lessons aware of their instructors

--- client/helpers/instructorList.js
+++ client/helpers/instructorList.js
@@ -2,4 +2,8 @@ Template.instructorList.helpers ({
 	instructors: function() {
 		return Instructors.find();
 	},
+	renderBreaks: function(lessonID) {
+		console.log(Lessons.findOne(lessonID));
+		lessonObj = Lessons.findOne(lessonID);
+	},
 });
