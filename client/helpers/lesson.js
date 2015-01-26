c92446644f36bbe4885d6ab153cc050c9a33b363
jjbandit <revelstokesnowboard@gmail.com>
2015-01-25 16:33:11 -0800

    add sanitizeLevel helper

--- client/helpers/lesson.js
+++ client/helpers/lesson.js
@@ -2,4 +2,11 @@ Template.lesson.helpers ({
 	getLesson: function(lessonID) {
 		return Lessons.findOne(lessonID);
 	},
+	sanitizeLevels: function(levelsAry) {
+		if (levelsAry[0] > 10) {
+			return 'Preschool ' + levelsAry;
+		} else {
+			return 'Swim Kids ' + levelsAry;
+		}
+	},
 });
