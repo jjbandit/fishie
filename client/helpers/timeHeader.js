fc3aee32dd48b21b2315b7434106155a633e0ee9
jjbandit <revelstokesnowboard@gmail.com>
2015-02-02 20:05:30 -0800

    fix timeHeader, add sanitizeHeaderTime method

--- client/helpers/timeHeader.js
+++ client/helpers/timeHeader.js
@@ -4,8 +4,24 @@ Template.timeHeader.helpers({
 		var lastLesson = Lessons.findOne({}, {sort: {lessonTime: -1}});
 		if (firstLesson) {
 			var firstTime = new Date(firstLesson.lessonTime[0]);
-			var lastTime = new Date(lastLesson.lessonTime[lastLesson.lessonTime.length - 1]);
-			return blocks = Fishie.getTimeBlocks(firstTime, lastTime);
+			var lastTime = new Date(lastLesson.lessonTime[0]);
+			blocks = Fishie.getTimeBlocks(firstTime, lastTime);
+			console.log(blocks);
+			return blocks;
 		}
 	},
+	sanitizeHeaderTime: function(timeObj) {
+		var minutes = timeObj.getMinutes();
+		var hours = timeObj.getHours();
+		var amPm = ' AM';
+		if (minutes == 0) {
+			minutes = '00';
+		}
+		if (hours > 12) {
+			hours = hours - 12;
+			amPm = ' PM';
+		}
+		saneTime = hours + ":" + minutes + amPm;
+		return saneTime;
+	}
 });
