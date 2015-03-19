0ed64ed8eb8a34641ad20ca9a1768f7d8e28d85c
jjbandit <revelstokesnowboard@gmail.com>
2015-03-19 09:12:29 -0700

    add instructor names

--- client/helpers/instructorList.js
+++ client/helpers/instructorList.js
@@ -10,5 +10,13 @@ Template.instructorList.helpers ({
 	},
 	getTrailingBreaks: function(lessonObj) {
 		return Fishie.getTrailingBreaks(lessonObj, false);
+	}
+});
+// Template.
+Template.instructorList.events ( {
+	'blur input.instructor-name': function () {
+		console.log(this);
+		console.log(event.target.value);
+		Fishie.setInstructorName(this, event.target.value);
 	},
 });
