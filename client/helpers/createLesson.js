42f404b148ce858f481b4e183710137a4bfdfeaf
jjbandit <revelstokesnowboard@gmail.com>
2015-04-11 15:03:16 -0700

    add preschool handling to parser

--- client/helpers/createLesson.js
+++ client/helpers/createLesson.js
@@ -47,7 +47,6 @@ Template.createLesson.events ({
 	'change .upload-lessons' : function(event) {
 		// We have to pass in a set Id because it's only available to us on the client
 		var setId = Router.current().params._id;
-		var startTime = new Date(1989, 0, 6, 2, 0, 0, 0);
 
 		var barcodeList = [];
 
@@ -56,48 +55,76 @@ Template.createLesson.events ({
 				complete: function(results, file) {
 					console.log(results);
 					$.each(results.data, function() {
+						// Here we're assuming that the csv we're feeding papaparse ALWAYS has the same format.
+						// Sounds sketchy at best considering CLASS..
 
 						// FIXME Not sure if we can do much about this
 						// Papaparse treats newlines as new rows and returns an extra empty object at the EOF
 						// so we have to check for this object :/
 						if (this.length === 1) { return; }
 
-						var instrID = new Meteor.Collection.ObjectID()._str;
-						var lessonID = new Meteor.Collection.ObjectID()._str;
-
-						// Here we're assuming that the csv we're feeding papaparse ALWAYS has the same format.
-						// Sounds sketchy at best considering CLASS..
-						var level = parseInt((this[10]).match(/\d+/));
-
-						// parseInt returns NaN when match doesn't encounter a number in the level index (this[10])
-						// so we know we're dealing with either a preschool level or some garbage
-						// TODO Handle preschool levels and private levels
-						if (isNaN(level)) {
-							return true;
-						}
-
-						// Get end time
-						var splitDate = this[22].split('-');
-						var startTime = new Date(splitDate[0]);
-						startTime.setDate(7);
-						startTime.setMonth(0);
-						startTime.setYear(1989);
-						var defaultDate = '7 Jan 1989 ';
-						var endTime = new Date(defaultDate.concat(splitDate[1]));
-						console.log(startTime);
-						console.log(endTime);
-						var diff = endTime.getTime() - startTime.getTime();
-						diff = (diff / 60000) - 1;
-
-						// Sanity check
+						// Sanity check; these should always be ints. If they're not we're probably dealing with garbage
 						var swimmers = parseInt((this[25]).match(/\d+/));
 						var barcode = parseInt((this[11]).match(/\d+/));
 						if (isNaN(swimmers) || isNaN(barcode)) {
 							return true;
 						}
 
+						// If we're dealing with a lesson we havn't created already continue
 						if (barcodeList.indexOf(barcode) === -1) {
 							barcodeList.push(barcode);
+
+							var instrID = new Meteor.Collection.ObjectID()._str;
+							var lessonID = new Meteor.Collection.ObjectID()._str;
+
+							// Class outputs non-standard date range formats so we have to do some pre-processing
+							// to create a pair of dates that make sense for us to use
+							var splitDate = this[22].split('-');
+							var startTime = new Date(splitDate[0]);
+							startTime.setDate(7);
+							startTime.setMonth(0);
+							startTime.setYear(1989);
+
+							// End time
+							var defaultDate = '7 Jan 1989 ';
+							var endTime = new Date(defaultDate.concat(splitDate[1]));
+							var diff = endTime.getTime() - startTime.getTime();
+							diff = (diff / 60000) - 1;
+
+							// parseInt returns NaN when match doesn't encounter a number in the level index (this[10])
+							// so we know we're dealing with a preschool level, private, or some garbage
+							var level = parseInt((this[10]).match(/\d+/));
+							if (isNaN(level)) {
+								// Set to the string we parsed in
+								var levelString = this[10];
+
+								// Check if we're dealing with a preschool level
+								if (levelString.indexOf('Preschool') > -1) {
+
+									// Loop thorugh each preschool level looking for the level we're adding
+									var preLevels = ['Starfish', 'Duck', 'Sea Turtle', 'Sea Otter', 'Salamander', 'Sunfish', 'Crocodile', 'Whale'];
+									$.each( preLevels, function() {
+
+										// This refers to the values in preLevels
+										if (levelString.indexOf(this) > -1){
+											// Add 10 so we get preschool levels
+											level = preLevels.indexOf(this.toString()) + 10;
+										}
+									});
+								}
+								if (levelString.indexOf('Private') > -1) {
+									console.log(level);
+									return true;
+								}
+							}
+
+
+							// If after all that processing level is still NaN break out
+							if (isNaN(level)){
+								return true;
+							}
+
+							// Finally create the object and send it to the server
 							var lessonObj = Fishie.parseLessonObject(lessonID, setId, level, false, swimmers, [0], startTime, diff);
 							lessonObj.barcode = barcode;
 							Meteor.call('createLesson', lessonObj);
