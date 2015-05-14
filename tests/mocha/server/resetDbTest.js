91f9af30bb3423313cecb4dfe3b332f1b813077e
jjbandit <revelstokesnowboard@gmail.com>
2015-05-13 17:43:37 -0700

    resetDb test

--- tests/mocha/server/resetDbTest.js
+++ tests/mocha/server/resetDbTest.js
@@ -1,17 +1,38 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-    describe("when calling resetDb", function(){
-      it("should clear all Collections", function(){
+
+		before( function () {
+			Lessons.remove({});
+			Instructors.remove({});
+			LessonSets.remove({});
+		});
+
+    describe("Calling resetDb", function(){
+			it("should clear all Collections", function(){
+
+				Lessons.insert({
+					_id: 'someLessonId',
+					someProperty: 'yay!'
+				});
+				Instructors.insert({
+					_id: 'someInstructorId',
+					someProperty: 'yay!'
+				});
+				LessonSets.insert({
+					_id: 'someSetId',
+					someProperty: 'yay!'
+				});
+
 				Meteor.call('resetDb');
 				var numLessons = Lessons.find().fetch().length;
-				chai.assert.equal(numLessons, 0, 'the Lessons Collection is not empty');
+				chai.assert.equal(0, numLessons, 'the Lessons Collection is not empty');
 
 				var numInstructors = Instructors.find().fetch().length;
-				chai.assert.equal(numInstructors, 0, 'the Instructors Collection is not empty');
+				chai.assert.equal(0, numInstructors, 'the Instructors Collection is not empty');
 
 				var numSets = LessonSets.find().fetch().length;
-				chai.assert.equal(numSets, 0, 'the Sets Collection is not empty');
-      });
+				chai.assert.equal(0, numSets, 'the Sets Collection is not empty');
+			});
     });
   });
 }
