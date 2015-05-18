9e68ccab43fec8c632498373057dbbd67c34ae19
jjbandit <revelstokesnowboard@gmail.com>
2015-05-18 16:22:36 -0700

    add proper route for set/create, reorder args to createLessonSet

--- methods/createLessonSet.js
+++ methods/createLessonSet.js
@@ -1,7 +1,7 @@
 Meteor.methods({
-	createLessonSet: function (setID, weekdays) {
-		LessonSets.insert({
-			_id: setID._str,
+	createLessonSet: function (weekdays, setId) {
+		return LessonSets.insert({
+			_id: setId,
 			weekdays: weekdays,
 			owner: Meteor.userId(),
 			instructorList: []
